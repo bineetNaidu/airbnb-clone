2e1057d92e88cd27613f9ab3764ba99eda6b2165
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-03 00:08:16 +0530

    add Card Component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import Card from './Card';
 
 // Statics
 import './Home.css';
@@ -8,6 +9,14 @@ const Home = () => {
   return (
     <div className="home">
       <Banner />
+
+      <div className="home__section">
+        <Card
+          src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
+          title="Online Experiences"
+          description="Unique activities we can do together, led by a world of hosts."
+        />
+      </div>
     </div>
   );
 };
